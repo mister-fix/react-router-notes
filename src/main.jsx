5eae72a8e0d5499c2e0742d21fb7c1517378786b
Stephen <hellostephenwm@gmail.com>
2024-10-12 13:40:36 -0400

    7-1: routing using the 'useState' hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,79 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const Home = () => (
+	<div>
+		<h2>Home</h2>
+	</div>
+);
+
+const Notes = () => (
+	<div>
+		<h2>Notes</h2>
+	</div>
+);
+
+const Users = () => (
+	<div>
+		<h2>Users</h2>
+	</div>
+);
+
+const App = () => {
+	const [page, setPage] = useState("home");
+
+	const toPage = (page) => (event) => {
+		event.preventDefault();
+		setPage(page);
+	};
+
+	const content = () => {
+		if (page === "home") {
+			return <Home />;
+		} else if (page === "notes") {
+			return <Notes />;
+		} else if (page === "users") {
+			return <Users />;
+		}
+	};
+
+	const padding = {
+		padding: 5,
+	};
+
+	return (
+		<div>
+			<div>
+				<a
+					href=""
+					onClick={toPage("home")}
+					style={padding}
+				>
+					home
+				</a>
+				<a
+					href=""
+					onClick={toPage("notes")}
+					style={padding}
+				>
+					notes
+				</a>
+				<a
+					href=""
+					onClick={toPage("users")}
+					style={padding}
+				>
+					users
+				</a>
+			</div>
+
+			{content()}
+		</div>
+	);
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
